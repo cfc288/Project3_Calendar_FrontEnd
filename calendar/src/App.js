d089bf7c1dfe3c08c049339045839cd08135c0a9
bpozniak <benjaminpozniak@gmail.com>
2021-10-25 13:14:45 -0400

    added calendar to app

--- calendar/src/App.js
+++ calendar/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ReactDOM from "react-dom";
+import Calendar from "./Calendar";
 
-let baseUrl = 'http://localhost:3003'
+
+let baseUrl = 'http://localhost:3003';
+
+const style = {
+  position: "relative",
+  margin: "50px auto"
+}
 
 class App extends Component {
   constructor(props) {
@@ -12,90 +20,81 @@ class App extends Component {
       lastName: '',
       month: '',
       day: '',
+      year: '',
       time: '',
       modalOpen: false,
       userLoggedIn: false,
-      admin: false
+      admin: false,
     }
-  }
-}
 
+} 
 
-
-
-loginUser = async (e) => {
-    console.log('loginUser')
-    e.preventDefault()
-    const url = baseUrl + '/users/login'
-    const loginBody = {
-      username: e.target.username.value,
-      password: e.target.password.value
-    }
-    try {
-
-      const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(loginBody),
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        credentials: "include"
-      })
-
-      console.log(response)
-      console.log("BODY: ",response.body)
-
-      if (response.status === 200) {
-        this.getAppointment()
-      }
-    }
-    catch (err) {
-      console.log('Error => ', err);
-    }
+ onDayClick = (e, day) => {
+    alert(day);
   }
 
-  register = async (e) => {
-    e.preventDefault()
-    const url = baseUrl + '/users/signup'
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify({
-          username: e.target.username.value,
-          password: e.target.password.value
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-      if (response.status === 200) {
-        this.getAppointment()
-      }
-    }
-    catch (err) {
-      console.log('Error => ', err);
-    }
-  }
+// loginUser = async (e) => {
+//     console.log('loginUser')
+//     e.preventDefault()
+//     const url = baseUrl + '/users/login'
+//     const loginBody = {
+//       username: e.target.username.value,
+//       password: e.target.password.value
+//     }
+//     try {
 
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//       const response = await fetch(url, {
+//         method: 'POST',
+//         body: JSON.stringify(loginBody),
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         credentials: "include"
+//       })
+
+//       console.log(response)
+//       console.log("BODY: ",response.body)
+
+//       if (response.status === 200) {
+//         this.getAppointment()
+//       }
+//     }
+//     catch (err) {
+//       console.log('Error => ', err);
+//     }
+//   }
+
+  // register = async (e) => {
+  //   e.preventDefault()
+  //   const url = baseUrl + '/users/signup'
+  //   try {
+  //     const response = await fetch(url, {
+  //       method: 'POST',
+  //       body: JSON.stringify({
+  //         username: e.target.username.value,
+  //         password: e.target.password.value
+  //       }),
+  //       headers: {
+  //         'Content-Type': 'application/json'
+  //       }
+  //     })
+  //     if (response.status === 200) {
+  //       this.getAppointment()
+  //     }
+  //   }
+  //   catch (err) {
+  //     console.log('Error => ', err);
+  //   }
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Calendar style={style} width="302px" 
+          onDayClick={(e, day)=> this.onDayClick(e, day)}/>     
+      </div>
+    );
+  }
 }
 
 export default App;
