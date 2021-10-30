a978d2ea7a1c9a43bf744d0658652b446302429f
Carmen Castro <85807725+cfc288@users.noreply.github.com>
2021-10-29 19:56:21 -0500

    updated

--- src/Appointment/index.js
+++ src/Appointment/index.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react'
 import './appointment.css'
 import img4 from "../img/shop.jpeg"
 
-const modalStyle = {
-  backgroundImage:"url(" + img4 + ")",
-}
+
 
 let baseUrl = 'http://localhost:3003';
 //'https://proj3-calendar-frontend.herokuapp.com/;'
@@ -25,35 +23,34 @@ export default class Appointment extends React.Component {
 
 
 		}
-		this.handleChange = this.handleChange.bind(this);
 	}
 
 
 
-	// handleChange = (e) => {
-	// 	// console.log('event.target.value',e.target.value)
-	// 	// console.log('event.target.name',e.target.name)
-	// 	this.setState({
-	// 		[e.target.name]: e.target.value
-		
-	// 	})
-	// }
-
-
-	handleChange(event) {
-		const target = event.target;
-		const value = target.type === 'checkbox' ? target.checked : target.value;
-		const name = target.name;
-	
+	handleChange = (e) => {
+		// console.log('event.target.value',e.target.value)
+		// console.log('event.target.name',e.target.name)
 		this.setState({
-		  [name]: value
-		});
-	  }
+			[e.target.name]: e.target.value
+		
+		})
+	}
 
 
-	  handleCloseModal = () => {
-		this.setState({ showModal: false }); 
-	  }
+	// handleBoxChange(event) {
+	// 	const target = event.target;
+	// 	const value = target.type === 'checkbox' ? target.checked : target.value;
+	// 	const name = target.name;
+	
+	// 	this.setState({
+	// 	  [name]: value
+	// 	});
+	//   }
+
+
+	handleCloseModal = () => {
+	this.setState({ showModal: false }); 
+	}
 
 //add a pop up on submit
 	
@@ -91,31 +88,93 @@ export default class Appointment extends React.Component {
 		console.log('this.state.apptTime insideRender: ' , this.state.apptTime)
 		return(
 			<div>
-			<div className="appointmentModal" style={modalStyle}>
-				<label> Appointment Date: {this.props.date}</label><br/ >
-				<br/ ><label> Appointment Time: {this.props.time}</label><br/ >
 				<form onSubmit={this.handleSubmit}>
 
-               <br/ ><label > Full Name: </label>
+				<label> appt date appt time</label>
+
+                <label > fullName: </label>
                 <input type='text' id='fullName' name='fullName' onChange={ this.handleChange } 
-                /><br/ >
+                />
 
-                <br/ ><label> Phone: </label>
-                <input type='text' id='phone' name='phone' onChange={this.handleChange}/><br/ >
+                <label> phone: </label>
+                <input type='text' id='phone' name='phone' onChange={this.handleChange}/>
 
-				<br/ ><label> Email: </label>
-                <input type='text' id='email' name='phone' onChange={this.handleChange}/><br/ >
+				<label> email: </label>
+                <input type='text' id='email' name='phone' onChange={this.handleChange}/>
 
                
 
-                <br/ ><input className="apptButton" type="submit" value="Add Appointment" />
+                <input type="submit" value="Add Appointment" />
             </form>
 			
-			{/*<label> over18: </label>
-			<input type='checkbox' id='over18' name='over18' onChange={this.handleChange}/>*/}
-			</div>
 			</div>
 			)
 	}
 
+
+
+
 }
+
+
+
+
+
+
+// <label> over18: </label>
+// <input type='checkbox' id='over18' name='over18' onChange={this.handleChange}/>
+
+// <tr key={i}>
+
+// <td> {bookmark.apptDate} </td>
+// <td> {bookmark.apptTime} </td>
+// <td> {bookmark.fullName} </td>
+// <td> {bookmark.phone} </td>
+// <td> {bookmark.email} </td>
+
+// </tr>
+
+
+// 	<tr>
+// 		<td onClick={() => {this.showEditForm(bookmark)} } onChange={this.handleChange}>  Change details on form
+// 		{
+// 			this.state.modalOpen 
+
+// 			&&
+// 			<div>
+// 			<form onSubmit={(e)=>{ this.props.handleSubmit(e, 
+// 				bookmark._id, 
+// 				this.state.updatedapptDate, this.state.updatedApptTime, 
+// 				this.state.updatedName,
+// 				this.state.updatedPhone
+// 				this.state.updatedEmail)} }>
+
+// 			<label>Title: </label>
+// 			<input type='text' id='title' name="updatedTitle"  onChange={this.handleChange} />
+
+// 			<label>Description: </label>
+// 			<input type='text' id='title' name="updatedDes"  onChange={this.handleChange} />
+
+
+// 			<label>Url: </label>
+// 			<input type='text' id='title' name="updatedUrl"  onChange={this.handleChange} />
+
+
+// 			<input type="submit" value="Edit" />
+
+// 			</form>
+// 			</div>
+	
+// 		}
+// 		</td>
+// </tr>
+                         
+//                     )})
+                    
+//                         }
+//                 </tbody>
+//             </table>
+//             <button onClick={this.closeModal} > Close Form</button>
+//         </div>
+        
+//     )}
